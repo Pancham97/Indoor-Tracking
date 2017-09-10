6826cf548c43704075f3bc3eb815a507a298f993
mrpancham97 <talktopancham@gmail.com>
2017-09-10 22:04:13 +0530

    Reverted the get-location.js back to the normal.

--- js/get-location.js
+++ js/get-location.js
@@ -33,71 +33,61 @@ var map, infoWindow;
           searchBox.setBounds(map.getBounds());
         });
 
-        // var markers = [];
-        $.ajax ({
-            url: 'demo.php',
-            type: 'GET',
-            success: function(data) {
-                var markers = JSON.stringify(data);
-                document.write(markers);
-            }
-        });
-
+        var markers = [];
         // Listen for the event fired when the user selects a prediction and retrieve
         // more details for that place.
-        // searchBox.addListener('places_changed', function() {
-        //   var places = searchBox.getPlaces();
-        //
-        //   if (places.length == 0) {
-        //     return;
-        //   }
-        //
-        //   // Clear out the old markers.
-        //   markers.forEach(function(marker) {
-        //     marker.setMap(null);
-        //   });
-        //
-        // //   // For each place, get the icon, name and location.
-        // //   var bounds = new google.maps.LatLngBounds();
-        // //   places.forEach(function(place) {
-        // //     if (!place.geometry) {
-        // //       console.log("Returned place contains no geometry");
-        // //       return;
-        // //     }
-        // //     var icon = {
-        // //       url: place.icon,
-        // //       size: new google.maps.Size(71, 71),
-        // //       origin: new google.maps.Point(0, 0),
-        // //       anchor: new google.maps.Point(17, 34),
-        // //       scaledSize: new google.maps.Size(25, 25)
-        // //     };
-        //   //
-        // //     // Create a marker for each place.
-        // //     markers.push(new google.maps.Marker({
-        // //       map: map,
-        // //       icon: icon,
-        // //       title: place.name,
-        // //       position: place.geometry.location,
-		// // 	  draggable: true
-        // //     }));
-        //   //
-        // //     if (place.geometry.viewport) {
-        // //       // Only geocodes have viewport.
-        // //       bounds.union(place.geometry.viewport);
-        // //     } else {
-        // //       bounds.extend(place.geometry.location);
-        // //     }
-        // //   });
-        //   map.fitBounds(bounds);
-        // });
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location,
+			  draggable: true
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
 
         //This is a pop-up if location is found.
-        infoWindow = new google.maps.InfoWindow, i;
-
-
+        infoWindow = new google.maps.InfoWindow;
 
         // Try HTML5 geolocation.
-        // if (navigator.geolocation) {
+        if (navigator.geolocation) {
 			console.log("Location services are supported!");
             navigator.geolocation.getCurrentPosition(function(position) {
                 var pos = {
@@ -114,11 +104,11 @@ var map, infoWindow;
                 //infoWindow.open(map);
                 map.setCenter(pos);
 
-                // var marker = new google.maps.Marker({
-				// 	draggable: true,
-                //     position: pos,
-                //     map: map
-                // });
+                var marker = new google.maps.Marker({
+					draggable: true,
+                    position: pos,
+                    map: map
+                });
 
                 // This code block is just for demo of 'click' event. Uncomment only if you are sure of what you are trying to do.
                 // google.maps.event.addListener(marker, 'click', function(event) {
@@ -134,38 +124,26 @@ var map, infoWindow;
 				// 	document.getElementById("longitude").innerHTML = event.latLng.lng();
                 // });
 
-				// google.maps.event.addListener(marker, 'dragend', function(event) {
-                //     map.setCenter(marker.getPosition());
-                //
-				// 	//Get new coordinates after dragging the marker on the map.
-				// 	console.log(event.latLng.lat());
-				// 	console.log(event.latLng.lng());
-                //
-				// 	//Change the values in the HTML.
-				// 	document.getElementById("latitude").innerHTML = event.latLng.lat();
-				// 	document.getElementById("longitude").innerHTML = event.latLng.lng();
-				// 	// infoWindow.open(map, marker);
-				// });
-                console.log(markers.length);
-                for(i = 0; i < markers.length; i++) {
-                    pos = new google.maps.LatLng(markers[i][1], markers[i][2]);
-                    bounds.extend(pos);
-                    marker = new google.maps.Marker({
-                        position: pos,
-                        map: map,
-                        title: markers[i][0]
-                    });
-                }
+				google.maps.event.addListener(marker, 'dragend', function(event) {
+                    map.setCenter(marker.getPosition());
 
-            // }, function() {
-            //     handleLocationError(true, infoWindow, map.getCenter());
-            // });
+					//Get new coordinates after dragging the marker on the map.
+					console.log(event.latLng.lat());
+					console.log(event.latLng.lng());
+
+					//Change the values in the HTML.
+					document.getElementById("latitude").innerHTML = event.latLng.lat();
+					document.getElementById("longitude").innerHTML = event.latLng.lng();
+					// infoWindow.open(map, marker);
+				});
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+			console.log("Browser does not support location services!");
         }
-        // } else {
-        //     // Browser doesn't support Geolocation
-        //     handleLocationError(false, infoWindow, map.getCenter());
-		// 	console.log("Browser does not support location services!");
-        // }
     }
 
     //new google.maps.event.addDomListener(window, "load", initMap());
